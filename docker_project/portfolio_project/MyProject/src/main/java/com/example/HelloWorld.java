d99cb533087d060a12e21c44efe39cb2d29b971d
PillaiSathya <Sathyachandru1312@gmail.com>
2025-05-27 12:19:55 +0400

    Add HelloWorld and test class

--- docker_project/portfolio_project/MyProject/src/main/java/com/example/HelloWorld.java
+++ docker_project/portfolio_project/MyProject/src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class HelloWorld {
-    public static String sayHello() {
-        return "Hello, Jenkins!";
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven project!");
+    }
+
+    public String sayHello() {
+        return "Hello, Sathya!";
     }
 }
--- docker_project/portfolio_project/MyProject/src/test/java/com/example/HelloWorldTest.java
+++ docker_project/portfolio_project/MyProject/src/test/java/com/example/HelloWorldTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
-
     @Test
     public void testSayHello() {
-        assertEquals("Hello, Jenkins!", HelloWorld.sayHello());
+        HelloWorld hello = new HelloWorld();
+        assertEquals("Hello, Sathya!", hello.sayHello());
     }
 }
