301e02c8dad80ce3864c67fafd1c8734a5863e62
PillaiSathya <Sathyachandru1312@gmail.com>
2025-05-28 19:40:04 +0400

    Fix pom.xml and add proper test config

--- docker_project/portfolio_project/MyProject/src/test/java/com/example/HelloWorldTest.java
+++ docker_project/portfolio_project/MyProject/src/test/java/com/example/HelloWorldTest.java
@@ -1,11 +1,10 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
     @Test
-    public void testHello() {
-        assertEquals(1, 1); // simple dummy test
+    void testOutput() {
+        System.out.println("Test is running...");
     }
 }
